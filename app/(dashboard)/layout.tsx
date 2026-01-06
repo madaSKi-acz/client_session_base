429d04af9ac1c15aa7f7ef431c46d74b2b1789f5
sok <sokgithub@gmail.com>
2026-01-06 15:45:22 +0700

    fixing h-screen overflow scroll

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,15 @@
 import Sidebar from "@/app/components/shared/Sidebar";
 import Navbar from "@/app/components/shared/Navbar";
 
-export default function DashboardLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
       <div className="flex flex-1 flex-col bg-gray-100/90">
         <Navbar />
-        <main className="p-6 h-screen relative overflow-hidden">{children}</main>
+        <main className="p-6 flex-1 relative overflow-hidden">
+          {children}
+        </main>
       </div>
     </div>
   );
